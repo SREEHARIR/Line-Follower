db25da02b4d67d51833cbc871ed578cd881c191a
R SREEHARI <sreehari.r96@gmail.com>
2015-11-27 14:28:46 +0530

    Basic Setup

---
+++ Arduino LineFollower/line_follower/line_follower.ino
@@ -0,0 +1,18 @@
+/* Pins */
+int sensor[8] = {13, 12, 11, 10, 9, 8, 1, 0}; /*left to right*/
+int el = 6;/*looking from back*/
+int er = 5;
+int mlp = 2;
+int mln = 3;
+int mrp = 4;
+int mrn = 7;
+
+/* Variables */
+int i, j;
+int num_sensor = 8;
+int sensor_values[8];
+int w[8] = { -4, -3, -2, -1, 1, 2, 3, 4};
+int result;
+int speedl;
+int speedr;
+int diff;
